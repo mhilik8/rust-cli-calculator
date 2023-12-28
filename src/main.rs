d662ee9c583b2187d41b219d585f50499e1fa0c3
Reuven <mhilik8@gmail.com>
2023-12-28 17:17:21 +0200

    new work with struct enum and trait

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+pub trait Math {
+    fn value(&self) -> i32;
+}
+
+enum Operation {
+    Add,
+    Sub
+}
+
+struct BinaryOperation {
+    left_operand: i32,
+    right_operand: i32,
+    action: Operation
+}
+
+impl Math for BinaryOperation{
+    fn value(&self) -> i32 {
+        match self.action {
+            Operation::Add => self.left_operand + self.right_operand,
+            Operation::Sub => self.left_operand - self.right_operand,
+        }
+    }
+}
+
 fn main() {
     println!("Hello to Rust CLI Calculator");
     let input_str = "5+6";
     let parsed = parse_math(input_str);
-    let results = parsed.0 + parsed.1;
-    println!("results: {}", results);
+    let my_binary_operation = BinaryOperation{
+        left_operand:parsed.0,
+        right_operand:parsed.1,
+        action: Operation::Add
+    };
+    let results = my_binary_operation.value();
+        println!("results: {}", results);
 }
 
 fn parse_math(to_parse: &str) -> (i32, i32){
